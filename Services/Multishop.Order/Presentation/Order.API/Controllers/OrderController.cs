956b8316be8c879a7dc64966b05d2b362e06d280
fatihtrkc <fatih_trkci96@hotmail.com>
2024-06-03 22:50:38 +0300

    Section6 OrderMicroservice order process was completed.

--- Services/Multishop.Order/Presentation/Order.API/Controllers/OrderController.cs
+++ Services/Multishop.Order/Presentation/Order.API/Controllers/OrderController.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Order.Application.Cqrs.Commands.OrderCommands;
+using Order.Application.Cqrs.Queries.OrderQueries;
 
 namespace Order.API.Controllers
 {
@@ -11,6 +13,51 @@ namespace Order.API.Controllers
         public OrderController(IMediator mediator)
         {
             this.mediator = mediator;
+        }
+
+        [HttpGet("Orders")]
+        public async Task<IActionResult> Orders()
+        {
+            var orderListQueryResponses = await mediator.Send(new OrderListQueryRequest());
+            if (orderListQueryResponses is null) return NotFound("Order info has not been system yet !");
+
+            return Ok(orderListQueryResponses);
+        }
+
+        [HttpGet("GetBy/{orderId}")]
+        public async Task<IActionResult> GetBy(Guid orderId)
+        {
+            var orderDetailQueryResponse = await mediator.Send(new OrderDetailQueryRequest() { Id = orderId });
+            if (orderDetailQueryResponse is null) return NotFound("Order info was not found !");
+
+            return Ok(orderDetailQueryResponse);
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add(OrderAddCommandRequest orderAddCommandRequest)
+        {
+            bool response = await mediator.Send(orderAddCommandRequest);
+            if (response) return BadRequest("This order info added process to system is unsuccess !");
+
+            return Ok("This order info was added successfully !");
+        }
+
+        [HttpDelete("Delete/{orderId}")]
+        public async Task<IActionResult> Delete(Guid orderId)
+        {
+            bool response = await mediator.Send(new OrderDeleteCommandRequest() { Id = orderId });
+            if (response) return BadRequest("This order info deleted process from system is unsuccess !");
+
+            return Ok("This order info was deleted successfully !");
+        }
+
+        [HttpPut("Update")]]
+        public async Task<IActionResult> Update(OrderUpdateCommandRequest orderUpdateCommandRequest)
+        {
+            bool response = await mediator.Send(orderUpdateCommandRequest);
+            if (response) return BadRequest("This order info updated process is unsuccess !");
+
+            return Ok("This order info was updated successfully !");
         }
     }
 }
